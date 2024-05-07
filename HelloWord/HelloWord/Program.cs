a108cc31fd9eca24cbe2d06a90b044bd5eeeecd2
Janaina Costa <88160110+janainaacosta@users.noreply.github.com>
2024-05-06 23:23:34 -0300

    primeiro commit

---
+++ HelloWord/HelloWord/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            var Enfeite = "=-=-=-=-=-=-=-";
+
+            Console.WriteLine(Enfeite);
+
+            Console.WriteLine("Hello World");
+            
+            Console.WriteLine(Enfeite);
+
+            Console.ReadLine();
+        }
+    }
+}
