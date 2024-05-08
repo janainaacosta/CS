af3090585a4713fec1dec063c977b35d03591c28
Janaina Costa <88160110+janainaacosta@users.noreply.github.com>
2024-05-07 22:40:24 -0300

    Add files via upload

---
+++ Constantes/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Constantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("escreva seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("seu nome é ");
+            Console.WriteLine(nome);
+
+            Console.ReadLine();
+        }
+    }
+}
