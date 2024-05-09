f7f9afb28d8e8318cd807158eaed09b9b80788ae
Janaina Costa <88160110+janainaacosta@users.noreply.github.com>
2024-05-08 23:11:49 -0300

    Add files via upload

---
+++ SaidaDeDados/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Condicionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int idade = 19;
+            double saldo = 100;
+            string nome = "ana";
+
+            Console.WriteLine("{0} tem {1} anos e seu saldo é {2} reais.", nome, idade, saldo);
+
+            Console.WriteLine($"{nome} tem {idade} anos e seu saldo é {saldo} reais.");
+
+            Console.WriteLine(nome + " tem " + idade + " anos e seu saldo é " + saldo + " reais");
+        }
+    }
+}
