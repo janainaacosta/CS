cfce3af68bb4d393fd6cf4920ed5ce6aefa39397
Janaina Costa <88160110+janainaacosta@users.noreply.github.com>
2024-05-09 22:38:00 -0300

    Add files via upload

---
+++ ConversaoImplicitaECasting/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConversaoImplicitaECasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            int b;
+
+            a = 1.5;
+            b = (int)a;
+
+            Console.WriteLine(b);
+
+
+
+        }
+    }
+}
