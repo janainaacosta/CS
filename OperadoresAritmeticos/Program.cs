db18cb923a5e1da96427953eeaa60b3ed2ad05fa
Janaina Costa <88160110+janainaacosta@users.noreply.github.com>
2024-05-09 22:39:13 -0300

    Add files via upload

---
+++ OperadoresAritmeticos/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace OperadoresAritmeticos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a = 1.0, b = -3.0, c = -4.0;
+
+            double delta = Math.Pow(b, 2.0) - 4 * a * c;
+
+            double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+
+
+            Console.WriteLine(x1);
+            Console.WriteLine(delta);
+        }
+    }
+}
